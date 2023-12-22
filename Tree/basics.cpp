1d49a1126096f9827e75968688bb91a98558d396
Amritesh <amriteshk778@gmail.com>
2023-12-22 23:00:10 +0530

    tree all traversal type

--- Tree/basics.cpp
+++ Tree/basics.cpp
@@ -62,6 +62,38 @@ void levelOrderTraversal(Node* root){
 }
 // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
 
+void buildFromLevelOrder(Node* root){
+    queue<Node*> q;
+    cout<<"Enter data for root" <<endl;
+
+    int data;
+    cin>>data;
+    root = new Node(data);
+    q.push(root);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        cout<<"Enter left node for:"<< temp -> left<<endl;
+        int leftData;
+        cin>>leftData;
+
+        if(leftData != -1){
+            temp->left = new Node(leftData);
+            q.push(temp->left);
+        }
+
+        cout<<"Enter right node for:"<< temp -> right<<endl;
+        int rightData;
+        cin>>rightData;
+
+        if(rightData != -1){
+            temp->right = new Node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
 
 void inorder(Node* root){
     if(root == NULL){
